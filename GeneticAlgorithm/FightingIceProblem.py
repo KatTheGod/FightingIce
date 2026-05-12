bf6c235c9c625011f37f569f41472d448c7ca655
Kat <Katlego.Kungoane@bbd.co.za>
2026-05-12 07:07:55 +0200

    cb+uq cluster run

--- GeneticAlgorithm/FightingIceProblem.py
+++ GeneticAlgorithm/FightingIceProblem.py
@@ -85,8 +85,8 @@ def evaluate_individual(x: np.ndarray, settings: IndividualSettings) -> np.ndarr
     return np.array(
         [
             -competitive_balance,
-            -excitement,
-            # -uniqueness_reward,
+            # -excitement,
+            -uniqueness_reward,
         ],
         dtype=np.float64,
     )
