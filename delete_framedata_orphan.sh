fd3af214842040ba176377738096b0c4f5f3d56e
KatTheGod <katlegok.chess@gmail.com>
2026-08-03 16:54:00 +0200

    using batch and no longer stampede for node limit

--- delete_framedata_orphan.sh
+++ delete_framedata_orphan.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -p bigbatch
+#SBATCH -p batch
 #SBATCH -J cull_r
 #SBATCH -o /home-mscluster/kkungoane/dare-fighting-ice/FightingIce/out/slurm.%N.%j.out
 #SBATCH -e /home-mscluster/kkungoane/dare-fighting-ice/FightingIce/err/slurm.%N.%j.err
@@ -7,9 +7,9 @@
 SRC="/home-mscluster/kkungoane/dare-fighting-ice/FightingIce/log/frameData_old_1805997_old_1734752"
 DEST="/home-mscluster/kkungoane/large_cull_delete"
 
-echo "[$(date '+%H:%M:%S')] Moving $SRC -> $DEST"
-mv "$SRC" "$DEST"
-echo "[$(date '+%H:%M:%S')] Move complete. Starting deletion."
+# echo "[$(date '+%H:%M:%S')] Moving $SRC -> $DEST"
+# mv "$SRC" "$DEST"
+# echo "[$(date '+%H:%M:%S')] Move complete. Starting deletion."
 
 find "$DEST" -mindepth 1 -maxdepth 1 | xargs -P 12 -r rm -rf
 rm -rf "$DEST"
