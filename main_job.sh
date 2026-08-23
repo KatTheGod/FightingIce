d395553afa397ea678e717fc12ea7d43dcddf851
KatTheGod <katlegok.chess@gmail.com>
2026-08-23 19:00:42 +0200

    batch pairwise 4, take 2 1 of 6 12 hour jobs to finish by 23 Monday 7pm

--- main_job.sh
+++ main_job.sh
@@ -2,7 +2,7 @@
 #SBATCH -p batch
 #SBATCH -c 6
 #SBATCH -N 8
-#SBATCH -J mse_pw5
+#SBATCH -J mse_pw4
 #SBATCH --ntasks-per-node=1
 #SBATCH -o /home-mscluster/kkungoane/dare-fighting-ice/FightingIce/out/slurm.%N.%j.out
 #SBATCH -e /home-mscluster/kkungoane/dare-fighting-ice/FightingIce/err/slurm.%N.%j.err
