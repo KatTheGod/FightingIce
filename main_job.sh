2792a62cbc7d88525aab2066a4dc0e332c7c0c75
KatTheGod <katlegok.chess@gmail.com>
2026-08-27 10:03:30 +0200

    bigbatch pairwise 15 job 2 of 2 to complete by: 6 pm Thursday

--- main_job.sh
+++ main_job.sh
@@ -2,7 +2,7 @@
 #SBATCH -p bigbatch
 #SBATCH -c 14
 #SBATCH -N 6
-#SBATCH -J mse_pw14
+#SBATCH -J mse_pw15
 #SBATCH --ntasks-per-node=1
 #SBATCH -o /home-mscluster/kkungoane/dare-fighting-ice/FightingIce/out/slurm.%N.%j.out
 #SBATCH -e /home-mscluster/kkungoane/dare-fighting-ice/FightingIce/err/slurm.%N.%j.err
@@ -18,12 +18,12 @@ export PYTHONPATH=$PYTHONPATH:$PROJECT_ROOT
 
 N_GEN=10
 GEN_PERIOD=6
-META_SPACE_INDEX=25
+META_SPACE_INDEX=26
 ENGINE_MULTIPLIER=4
 NO_MATCHES=8
 N_PARTITIONS=7 # Don't change me often
 N_NEIGHBORS=8 # Don't change me often
-EXPERIMENT_NAME="mse_pairwise_14" # NB!!
+EXPERIMENT_NAME="mse_pairwise_15" # NB!!
 PARTITION="bigbatch"
 NODES=6
 CORES=14
