368face88f5266eb382d03c5aca6d3f135a8f9c1
KatTheGod <katlegok.chess@gmail.com>
2026-09-08 16:57:58 +0200

    fixed broken variable objective set phenotype run

--- main_job.sh
+++ main_job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH -p stampede
-#SBATCH -c 16
-#SBATCH -N 5
-#SBATCH -J exp_gt
+#SBATCH -p bigbatch
+#SBATCH -c 14
+#SBATCH -N 6
+#SBATCH -J exp_pt
 #SBATCH --ntasks-per-node=1
 #SBATCH -o /home-mscluster/kkungoane/dare-fighting-ice/FightingIce/out/slurm.%N.%j.out
 #SBATCH -e /home-mscluster/kkungoane/dare-fighting-ice/FightingIce/err/slurm.%N.%j.err
@@ -16,20 +16,19 @@ mkdir -p dask_schedulers
 PROJECT_ROOT="/home-mscluster/kkungoane/dare-fighting-ice/FightingIce"
 export PYTHONPATH=$PYTHONPATH:$PROJECT_ROOT
 
-OBJECTIVES="cb_gt"
+OBJECTIVES="cb_pt"
 N_GEN=10
 GEN_PERIOD=6
 META_SPACE_INDEX=32
-ENGINE_MULTIPLIER=5
-NO_MATCHES=5
+ENGINE_MULTIPLIER=4
+NO_MATCHES=6
 # N_PARTITIONS=7 # Don't change me often
 N_PARTITIONS=35 # specifically to get 46 with 2 objectives
 N_NEIGHBORS=8 # Don't change me often
-EXPERIMENT_NAME="concat_genotype" # NB!!
-PARTITION="stampede"
-NODES=5
-CORES=16
-
+EXPERIMENT_NAME="concat_phenotype" # NB!!
+PARTITION="bigbatch"
+NODES=6
+CORES=14
 # Computing nthreads
 THREADS_PER_WORKER=$(( (CORES / (ENGINE_MULTIPLIER * 3)) > 0 ? (CORES / (ENGINE_MULTIPLIER * 3)) : 1 ))
 
